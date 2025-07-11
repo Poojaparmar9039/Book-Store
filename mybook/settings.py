79cec9c68539ace6d6a6dd11814f2813caf9b089
Poojaparmar9039 <157222585+Poojaparmar9039@users.noreply.github.com>
2025-07-11 12:33:00 +0530

    Update settings.py

--- mybook/settings.py
+++ mybook/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d^wj85=xpd+_5p^!#m8=9-z!xx4tyu3zcwkk12=h890t+w$%^p'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['bookstore-g1lx.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
